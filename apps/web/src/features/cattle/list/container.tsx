33b90cc5557c1269f18f6a1c98bb76f6fcbbfb83
katsuya6152 <katsuya.6152@gmail.com>
2025-05-25 18:29:45 +0900

    fix(web): APIレスポンスの型を生成するように変更

--- apps/web/src/features/cattle/list/container.tsx
+++ apps/web/src/features/cattle/list/container.tsx
@@ -1,14 +1,14 @@
-import { fetchCattleList } from "@/services/cattleService";
+import { GetCattleList } from "@/services/cattleService";
 import { notFound } from "next/navigation";
 import { CattleListPresentation } from "./presentational";
 
 export const runtime = "edge";
 
 export default async function CattleListContainer() {
-	let cattleList: Awaited<ReturnType<typeof fetchCattleList>>;
+	let cattleList: Awaited<ReturnType<typeof GetCattleList>>;
 
 	try {
-		cattleList = await fetchCattleList();
+		cattleList = await GetCattleList();
 	} catch (e) {
 		console.error(e);
 		notFound();
