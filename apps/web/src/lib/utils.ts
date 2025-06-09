0043b32d1aa871bef7f2024bb64a7dd96692d4c4
katsuya6152 <katsuya.6152@gmail.com>
2025-06-09 22:52:02 +0900

    feat(web):詳細画面仮実装

--- apps/web/src/lib/utils.ts
+++ apps/web/src/lib/utils.ts
@@ -4,3 +4,28 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
+
+export const getGrowthStage = (
+	growthStage:
+		| "CALF"
+		| "GROWING"
+		| "FATTENING"
+		| "FIRST_CALVED"
+		| "MULTI_PAROUS"
+		| null,
+) => {
+	switch (growthStage) {
+		case "CALF":
+			return "仔牛";
+		case "GROWING":
+			return "育成牛";
+		case "FATTENING":
+			return "肥育牛";
+		case "FIRST_CALVED":
+			return "初産牛";
+		case "MULTI_PAROUS":
+			return "経産牛";
+		default:
+			return "不明";
+	}
+};
