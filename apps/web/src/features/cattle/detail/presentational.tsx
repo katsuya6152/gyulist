664bfd233238c8743249df883528658c6dacaa6f
katsuya6152 <katsuya.6152@gmail.com>
2025-06-09 23:10:00 +0900

    feat(web):詳細画面にスワイプ導入

--- apps/web/src/features/cattle/detail/presentational.tsx
+++ apps/web/src/features/cattle/detail/presentational.tsx
@@ -1,5 +1,9 @@
+"use client";
+
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import type { GetCattleDetailResType } from "@/services/cattleService";
+import useEmblaCarousel from "embla-carousel-react";
+import { useCallback, useEffect, useState } from "react";
 import { BasicInfo } from "./components/basic-info";
 import { CattleDetailHeader } from "./components/hedear";
 
@@ -8,53 +12,91 @@ type Props = {
 };
 
 export default function CattleDetailPresentation({ cattle }: Props) {
+	const [selectedTab, setSelectedTab] = useState("basic");
+	const [emblaRef, emblaApi] = useEmblaCarousel({
+		align: "start",
+		skipSnaps: false,
+	});
+
+	const onSelect = useCallback(() => {
+		if (!emblaApi) return;
+		setSelectedTab(
+			["basic", "bloodline", "breeding", "history"][
+				emblaApi.selectedScrollSnap()
+			],
+		);
+	}, [emblaApi]);
+
+	useEffect(() => {
+		if (!emblaApi) return;
+		onSelect();
+		emblaApi.on("select", onSelect);
+		return () => {
+			emblaApi.off("select", onSelect);
+		};
+	}, [emblaApi, onSelect]);
+
+	const scrollTo = useCallback(
+		(index: number) => {
+			if (!emblaApi) return;
+			emblaApi.scrollTo(index);
+		},
+		[emblaApi],
+	);
+
 	return (
 		<div className="p-4">
 			{cattle ? (
 				<div className="mt-4 flex flex-col gap-4">
 					<CattleDetailHeader cattle={cattle} />
 
-					<Tabs defaultValue="basic" className="w-full">
+					<Tabs value={selectedTab} className="w-full">
 						<TabsList className="grid w-full grid-cols-4">
-							<TabsTrigger value="basic">基本情報</TabsTrigger>
-							<TabsTrigger value="bloodline">血統</TabsTrigger>
-							<TabsTrigger value="breeding">繁殖</TabsTrigger>
-							<TabsTrigger value="history">活動履歴</TabsTrigger>
+							<TabsTrigger value="basic" onClick={() => scrollTo(0)}>
+								基本情報
+							</TabsTrigger>
+							<TabsTrigger value="bloodline" onClick={() => scrollTo(1)}>
+								血統
+							</TabsTrigger>
+							<TabsTrigger value="breeding" onClick={() => scrollTo(2)}>
+								繁殖
+							</TabsTrigger>
+							<TabsTrigger value="history" onClick={() => scrollTo(3)}>
+								活動履歴
+							</TabsTrigger>
 						</TabsList>
 
-						<TabsContent value="basic">
-							<BasicInfo cattle={cattle} />
-						</TabsContent>
-						<TabsContent value="bloodline">
-							Bloodline
-							<BasicInfo cattle={cattle} />
-							{/* <Bloodline cattle={cattle} /> */}
-						</TabsContent>
-						<TabsContent value="breeding">
-							Breeding
-							<BasicInfo cattle={cattle} />
-							{/* <Breeding
-								statusData={breedingStatus?.data}
-								summaryData={breedingSummary?.data}
-							/> */}
-						</TabsContent>
-						<TabsContent value="history">
-							History
-							<BasicInfo cattle={cattle} />
-							{/* <History eventData={events?.data} /> */}
-						</TabsContent>
+						<div className="overflow-hidden" ref={emblaRef}>
+							<div className="flex">
+								<div className="flex-[0_0_100%] min-w-0">
+									BasicInfo
+									<BasicInfo cattle={cattle} />
+								</div>
+								<div className="flex-[0_0_100%] min-w-0">
+									Bloodline
+									<BasicInfo cattle={cattle} />
+									{/* <Bloodline cattle={cattle} /> */}
+								</div>
+								<div className="flex-[0_0_100%] min-w-0">
+									Breeding
+									<BasicInfo cattle={cattle} />
+									{/* <Breeding
+										statusData={breedingStatus?.data}
+										summaryData={breedingSummary?.data}
+									/> */}
+								</div>
+								<div className="flex-[0_0_100%] min-w-0">
+									History
+									<BasicInfo cattle={cattle} />
+									{/* <History eventData={events?.data} /> */}
+								</div>
+							</div>
+						</div>
 					</Tabs>
 
 					<div className="flex justify-center gap-2 text-xs text-gray-500">
-						<p>
-							登録日時:
-							{cattle.createdAt}
-						</p>
-						/
-						<p>
-							更新日時:
-							{cattle.updatedAt}
-						</p>
+						<p>登録日時: {cattle.createdAt}</p>/
+						<p>更新日時: {cattle.updatedAt}</p>
 					</div>
 				</div>
 			) : (
