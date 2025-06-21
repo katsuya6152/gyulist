6b725c72321f617f38f4097a4c6a4b5f8394020d
katsuya6152 <katsuya.6152@gmail.com>
2025-06-21 18:13:22 +0900

    feat(web): 牛削除機能実装

--- apps/web/src/features/cattle/new/container.tsx
+++ apps/web/src/features/cattle/new/container.tsx
@@ -1,7 +1,5 @@
 import { CattleNewPresentation } from "./presentational";
 
 export default async function CattleNewContainer() {
-	// TODO: POST /cattle
-	// TODO: propsとしてonSubmitを渡す（成功時の表示とリダイレクト）
 	return <CattleNewPresentation />;
 }
